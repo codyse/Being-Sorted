2c8de8bdbeeb7d59c2550dc948d3fd7ee1d0aed2
Kyle Gimple <kylegimple@gmail.com>
2021-08-06 17:24:37 -0400

    Stub for SearchInventory

---
+++ SearchInventory.java
@@ -0,0 +1,20 @@
+package csu.csci325;
+
+/**
+*
+* @author Kyle Gimple 
+*/
+
+public class SearchInventory {
+	
+	private String inventoryList;
+	
+	public void searchByName() {
+		
+	}
+	
+	public void searchByDescription() {
+		
+	}
+
+}
