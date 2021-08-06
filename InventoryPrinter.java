ecab2a69c568a9b3352bc8fe0d26f9fc0d8f2283
codyse <87039813+codyse@users.noreply.github.com>
2021-08-06 11:29:44 -0400

    Add files via upload

--- InventoryPrinter.java
+++ InventoryPrinter.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package csu.csci325;
 
 /**
- *
+ * This class allows the user to print the inventory to the screen, or print the
+ * inventory to a separate file
  * @author Cody
  */
 public class InventoryPrinter {
+    public void printInventory(Inventory inventory) {
+        System.out.println("Name \t Amount \t Description");
+        System.out.print ("Name: " + inventory.getName() 
+                + " Amount: " + inventory.getAmount() 
+                + " Description: " + inventory.getDescription());
+    }
     
+    public void printToDocument(String fileName) {
+        
+    }
 }
+
