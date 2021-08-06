2cf5975f97a436c4713225375210857e800226a8
codyse <87039813+codyse@users.noreply.github.com>
2021-08-06 12:02:07 -0400

    Add files via upload

--- InventoryPrinter.java
+++ InventoryPrinter.java
@@ -1,5 +1,7 @@
 package csu.csci325;
-
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
 /**
  * This class allows the user to print the inventory to the screen, or print the
  * inventory to a separate file
@@ -8,15 +10,22 @@ package csu.csci325;
 public class InventoryPrinter {
     public void printInventory(Inventory inventory) {
         System.out.println("Name \t Company Name \t Amount \t Location\t Description");
-        System.out.print ("Name: " + inventory.getName()
-                + " Company Name: " + inventory.getCompanyName()
-                + " Amount: " + inventory.getAmount() 
-                + " Location: " + inventory.getLocation()
-                + " Description: " + inventory.getDescription());
+        for(int i = 0; i < inventory.items.length(); i++) {
+            System.out.print ("Name: " + inventory.getName()
+                    + " Company Name: " + inventory.getCompanyName()
+                    + " Amount: " + inventory.getAmount() 
+                    + " Location: " + inventory.getLocation()
+                    + " Description: " + inventory.getDescription());
+        }
     }
     
-    public void printToDocument(String fileName) {
+    public void printToDocument(String fileName, Inventory inventory) throws IOException {
+        FileOutputStream fileStream = new FileOutputStream(fileName); 
+        PrintWriter outFS = new PrintWriter(fileStream);
         
+        outFS.println(printInventory(inventory));
+        
+        outFS.close();
     }
 }
 
