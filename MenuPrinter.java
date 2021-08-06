25434e2d301765c1fd3e94bd783dba617cd4f028
maysiamiles <59926366+maysiamiles@users.noreply.github.com>
2021-08-05 23:05:43 -0400

    Add files via upload

---
+++ MenuPrinter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menuprinter;
+import java.util.*;
+
+/**
+ *
+ * @author anjolee 
+ */
+public class MenuPrinter {
+    
+
+
+    public void printStartMenu(){
+
+    }
+
+    public void printOptions(){
+
+    }
+    public static void main(String[] args) {
+            Scanner scnr = new Scanner (System.in);
+
+    }
+}    
+    
+
