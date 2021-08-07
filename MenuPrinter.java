27498bb29e372dc9f4bbf083df1b934e1f9d5a3e
maysiamiles <59926366+maysiamiles@users.noreply.github.com>
2021-08-07 14:21:25 -0400

    Add files via upload

--- MenuPrinter.java
+++ MenuPrinter.java
@@ -4,34 +4,81 @@
  * and open the template in the editor.
  */
 package menuprinter;
-import java.util.*;
+import java.util.Scanner;
 
 /**
  *
  * @author anjolee 
  */
 public class MenuPrinter {
-    
-
-
+    char optionInput;
     public void printStartMenu(){
-
+        System.out.println("Hello User, let's get you started!");
+        System.out.println("First, you'll be prompted to choose an option.");
+        System.out.println("Then, We'll gather all the information you give us to keep in our system!");  
+        System.out.println("You can tell me anything you want..I won't judge!");
+        System.out.println();
+        System.out.println();
     }
 
     public void printOptions(){
+        Scanner input = new Scanner (System.in);
+        String userInput = input.nextLine();
 
-    }
+        this.optionInput = optionInput;
+        System.out.println("/t Here are your options: /t " );
+        System.out.println("p.) Print the inventory");
+        System.out.println("a.) Add items");
+        System.out.println("u.) Subtract items");
+        System.out.println("s.) Search for items(using description)");
+        System.out.println("o.) Sort items");
+        System.out.println("q.) Change quality of items" );
+        System.out.println();
+        System.out.println();
+        System.out.println("What's your choice? ");
+        /*if (userInput != 'p'){
+      Cody's Class
+        }
+        */
+        
+        /*if (userInput != 'a'){
+      Alan's Class
+        }
+        */
+        
+        /*if (userInput != 'u'){
+      Alan's Class
+        }
+        */
+        
+        /*if (userInput != 's'){
+      Kyle' Class
+        }
+        */
+        
+        /*if (userInput != 'o'){
+      Matt's Class
+        }
+        */
+        
+        /*if (userInput != 'q'){
+      James's Class
+        }
+        */
+        
+        //else(
+        //System.out.println("I'm sorry, I didn't get that. Can you try again");
+        //}
+        
+    } 
     public static void main(String[] args) {
-        Scanner scnr = new Scanner (System.in);
         
-                
-        Inventory inventory = new Inventory();
+        MenuPrinter intro = new MenuPrinter();
+
+        intro.printStartMenu();
+
+        intro.printOptions();        
         
-//        TODO: Implement these classes per design doc
-//        MenuPrinter menu = new MenuPrinter();
-//        InventoryPrinter inventoryPrinter = new InventoryPrinter(inventory);
-//        InventorySearcher searcher = new InventorySearcher(inventory);
-//        InventorySorter sorter = new InventorySorter(inventory);
 
     }
 }    
