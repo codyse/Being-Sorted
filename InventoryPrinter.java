e227953bb5b8457d388fd4d9524e6409868a711d
codyse <87039813+codyse@users.noreply.github.com>
2021-08-07 10:58:44 -0400

    Add files via upload

--- InventoryPrinter.java
+++ InventoryPrinter.java
@@ -6,36 +6,35 @@ import java.io.IOException;
  * This class allows the user to print the inventory to the screen, or print the
  * inventory to a separate file
  * @author Cody
- * @author Kyle Gimple
  */
 public class InventoryPrinter {
     public void printInventory(Inventory inventory) {
-        System.out.println("Name          Company Name          Amount          Location          Description");
-        for(int i = 0; i < inventory.items.size(); i++) { // Changed inventory.items.length to inventory.items.size
-            System.out.print ("Name: " + inventory.getName()
-                    + " Company Name: " + inventory.getCompanyName()
-                    + " Amount: " + inventory.getAmount() 
-                    + " Location: " + inventory.getLocation()
-                    + " Description: " + inventory.getDescription());
-        }
+        System.out.println(inventoryFormat(inventory));
     }
     
     public void printToDocument(String fileName, Inventory inventory) throws IOException {
         FileOutputStream fileStream = new FileOutputStream(fileName); 
         PrintWriter outFS = new PrintWriter(fileStream);
         
-        // outFS.println(printInventory(inventory));
-        // replaced call to printInventory with the actual text value, as the printInventory doesn't have a return value.
-        outFS.println("Name          Company Name          Amount          Location          Description");
-        for(int i = 0; i < inventory.items.size(); i++) {
-            outFS.print ("Name: " + inventory.getName()
-                    + " Company Name: " + inventory.getCompanyName()
-                    + " Amount: " + inventory.getAmount() 
-                    + " Location: " + inventory.getLocation()
-                    + " Description: " + inventory.getDescription());
-        }
+        
+        outFS.println();
         
         outFS.close();
     }
+    
+    public String inventoryFormat (Inventory inventory) {
+        String formation;
+        
+        formation = "Name          Company Name          Amount          Location          Description";
+        
+        for(int i = 0; i < inventory.items.size(); i++) {
+            formation = formation + "Name of item: " + inventory.items.get(i).getName() + '\n'
+                    + "Company Name: " + inventory.getCompanyName() + '\n'
+                    + "Amount: " + inventory.items.get(i).getAmount() + '\n'
+                    + "Location: " + inventory.getLocation() + '\n'
+                    + "Description: " + inventory.items.get(i).getDescription() + '\n';
+        }
+        
+        return formation;
+    }
 }
-
