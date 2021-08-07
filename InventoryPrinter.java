787a425cf2fa1b598a4ec7c8eaee1c4bec9c2b76
Kyle Gimple <kylegimple@gmail.com>
2021-08-07 08:47:23 -0400

    Fixed InventoryPrinter
    In InventoryPrinter changed printInventory to get correct size of inventory, and in printToDocument replaced function call to printInventory with a copy of the code of printInventory as printInventory does not have a return value.

--- InventoryPrinter.java
+++ InventoryPrinter.java
@@ -6,11 +6,12 @@ import java.io.IOException;
  * This class allows the user to print the inventory to the screen, or print the
  * inventory to a separate file
  * @author Cody
+ * @author Kyle Gimple
  */
 public class InventoryPrinter {
     public void printInventory(Inventory inventory) {
         System.out.println("Name          Company Name          Amount          Location          Description");
-        for(int i = 0; i < inventory.items.length(); i++) {
+        for(int i = 0; i < inventory.items.size(); i++) { // Changed inventory.items.length to inventory.items.size
             System.out.print ("Name: " + inventory.getName()
                     + " Company Name: " + inventory.getCompanyName()
                     + " Amount: " + inventory.getAmount() 
@@ -23,7 +24,16 @@ public class InventoryPrinter {
         FileOutputStream fileStream = new FileOutputStream(fileName); 
         PrintWriter outFS = new PrintWriter(fileStream);
         
-        outFS.println(printInventory(inventory));
+        // outFS.println(printInventory(inventory));
+        // replaced call to printInventory with the actual text value, as the printInventory doesn't have a return value.
+        outFS.println("Name          Company Name          Amount          Location          Description");
+        for(int i = 0; i < inventory.items.size(); i++) {
+            outFS.print ("Name: " + inventory.getName()
+                    + " Company Name: " + inventory.getCompanyName()
+                    + " Amount: " + inventory.getAmount() 
+                    + " Location: " + inventory.getLocation()
+                    + " Description: " + inventory.getDescription());
+        }
         
         outFS.close();
     }
