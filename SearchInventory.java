018b62f6c756f94e7463b4fce2cbeb4910d90ea4
Kyle Gimple <kylegimple@gmail.com>
2021-08-06 23:36:25 -0400

    Basic searchByName
    Basic structure for searchByName. Doesn't have a return value yet.

--- SearchInventory.java
+++ SearchInventory.java
@@ -1,4 +1,5 @@
 package csu.csci325;
+import java.util.ArrayList;
 
 /**
 *
@@ -9,8 +10,24 @@ public class SearchInventory {
 	
 	private String inventoryList;
 	
-	public void searchByName() {
+	public void searchByName(String search, ArrayList<Item> items) {
+		// Initialize variables
+		int numItems = items.size();
+		int i = 0;
+		int j = 0;
+		int[] matches = new int[i];
+		String name;
 		
+		// Loops through each item in the ArrayList items. If the name matches the search parameter it
+		//		stores the indices of the item in the matches array.
+		while (i < numItems) {
+			name = items.get(i).getName();
+			if (name.contains(search)) {
+				matches[j] = i;
+				j++;
+			}
+			i++;
+		}
 	}
 	
 	public void searchByDescription() {
