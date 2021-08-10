148c52364338148a281639ee133bfbf28285d306
James Dixon <notjamesdixon@gmail.com>
2021-08-09 20:20:06 -0400

    format code and put source files under csu/csi325 directory

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Being-Sorted
+
 Inventory application created by CSCI 325 summer 2 2021 students
--- csu/csci325/SearchInventory.java
+++ csu/csci325/SearchInventory.java
@@ -1,13 +1,13 @@
 package csu.csci325;
+
 import java.util.stream.IntStream;
 
 /**
-*
-* @author Kyle Gimple 
-*/
-
+ *
+ * @author Kyle Gimple
+ */
 public class SearchInventory {
-	
+
 	public static int[] searchByName(String search, Inventory inventory) {
 		// Initialize variables
 		int numItems = inventory.items.size();
@@ -15,9 +15,10 @@ public class SearchInventory {
 		int j = 0;
 		int[] matches = new int[numItems];
 		String name;
-		
-		// Loops through each item in the ArrayList items. If the name matches the search parameter it
-		//		stores the indices of the item in the matches array.
+
+		// Loops through each item in the ArrayList items. If the name matches the
+		// search parameter it
+		// stores the indices of the item in the matches array.
 		while (i < numItems) {
 			name = inventory.items.get(i).getName();
 			if (name.contains(search)) {
@@ -28,8 +29,9 @@ public class SearchInventory {
 		}
 		return matches;
 	}
-	
-	// only use this if you want to search description without searching by name; otherwise use completeSearch.
+
+	// only use this if you want to search description without searching by name;
+	// otherwise use completeSearch.
 	public static int[] searchByDescription(String search, Inventory inventory) {
 		// Initialize variables
 		int numItems = inventory.items.size();
@@ -37,9 +39,10 @@ public class SearchInventory {
 		int j = 0;
 		int[] matches = new int[numItems];
 		String name;
-		
-		// Loops through each item in the ArrayList items. If the name matches the search parameter it
-		//		stores the indices of the item in the matches array.
+
+		// Loops through each item in the ArrayList items. If the name matches the
+		// search parameter it
+		// stores the indices of the item in the matches array.
 		while (i < numItems) {
 			name = inventory.items.get(i).getDescription();
 			if (name.contains(search)) {
@@ -50,12 +53,14 @@ public class SearchInventory {
 		}
 		return matches;
 	}
+
 	public static int[] completeSearch(String search, Inventory inventory) {
 		int[] nameMatch = searchByName(search, inventory);
 		int[] descriptionMatch = searchByDescription(search, inventory);
-		int[] mergedArray = IntStream.concat(IntStream.of(nameMatch), IntStream.of(descriptionMatch)).distinct().sorted().toArray();
-		
+		int[] mergedArray = IntStream.concat(IntStream.of(nameMatch), IntStream.of(descriptionMatch)).distinct()
+				.sorted().toArray();
+
 		return mergedArray;
 	}
-	
+
 }
