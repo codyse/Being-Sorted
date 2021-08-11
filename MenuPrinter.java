915f89131da0a275b28672e8d63b7a69306dc3a6
James Dixon <notjamesdixon@gmail.com>
2021-08-11 10:59:30 -0400

    fix compiler errors

--- MenuPrinter.java
+++ MenuPrinter.java
@@ -8,22 +8,22 @@ import java.util.Scanner;
 
 /**
  *
- * @author anjolee 
+ * @author anjolee
  */
 public class MenuPrinter {
     char optionInput;
-    
+
     public void printStartMenu(){
         System.out.println("Hello User, let's get you started!");
         System.out.println("First, you'll be prompted to choose an option.");
-        System.out.println("Then, We'll gather all the information you give us to keep in our system!");  
+        System.out.println("Then, We'll gather all the information you give us to keep in our system!");
         System.out.println("You can tell me anything you want..I won't judge!");
         System.out.println();
         System.out.println();
     }
 
     public boolean printOptions (Scanner input, Inventory inventory, InventoryPrinter printer){
-        
+
         System.out.println("\t Here are your options: \t " );
         System.out.println("p.) Print the inventory");
         System.out.println("a.) Add items");
@@ -36,20 +36,20 @@ public class MenuPrinter {
         System.out.println();
         System.out.println();
         System.out.println("What's your choice? ");
-        
+
         optionInput = input.nextLine().charAt(0);
         optionInput = Character.toLowerCase(optionInput);
-        
+
         if (optionInput == 'u') {
         	int selection;
         	//print inventory
             printer.printInventory(inventory);
-            
+
             //make selection
             System.out.println("Select an Item to remove: ");
             selection = input.nextInt();
             input.nextLine();
-            
+
             //remove selection
             inventory.items.remove(selection - 1);
         }
@@ -60,17 +60,17 @@ public class MenuPrinter {
             Item item = new Item();
             String newName, newDiscription;
             int newAmount;
-            
+
             System.out.print("Enter the product name: ");
             newName = input.nextLine();
-            
+
             System.out.print("Enter a discription for the product: ");
             newDiscription = input.nextLine();
-             
+
             System.out.print("Enter how much of the product is available (in intager form): ");
             newAmount = input.nextInt();
             input.nextLine();
-            
+
             inventory.items.add(item);
             inventory.items.get(inventory.items.size() - 1).setName(newName);
             inventory.items.get(inventory.items.size() - 1).setDescription(newDiscription);
@@ -116,22 +116,22 @@ public class MenuPrinter {
             String itemTwo = "";
             switch(sortInput) {
             case 'n' :
-                sortByName(Inventory inventory);
+                SortInventory.sortByName(inventory);
                 break;
             case 'a' :
-                sortByAmountAcend(Inventory inventory);
+                SortInventory.sortByAmountAcend(inventory);
                 break;
             case 'd' :
-                sortByAmountDecend(Inventory inventory);
+                SortInventory.sortByAmountDecend(inventory);
                 break;
             case 's' :
                 System.out.print("Select the name of the first item: ");
-                itemOne = scnr.nextLine();
+                itemOne = input.nextLine();
                 System.out.println("");
                 System.out.print("Select the name of the second item: ");
-                itemTwo = scnr.nextLine();
+                itemTwo = input.nextLine();
                 System.out.println("");
-                switchPosition(Inventory inventory, itemOne, itemTwo);
+                SortInventory.switchPosition(inventory, itemOne, itemTwo);
                 break;
             default :
                 System.out.println("Sorry, that is not a valid selection.");
@@ -139,13 +139,13 @@ public class MenuPrinter {
         }
         else if (optionInput == 'c') {
         	String location, companyName;
-        	
+
         	System.out.print("Enter the location of the product: ");
             location = input.nextLine();
-            
+
             System.out.print("Enter the company that sells the product: ");
             companyName = input.nextLine();
-            
+
             inventory.setCompanyName(companyName);
             inventory.setLocation(location);
         }
@@ -155,11 +155,11 @@ public class MenuPrinter {
         else {
         		System.out.println("I'm sorry, I didn't get that. Can you try again");
         }
-        
+
         return true;
-    } 
+    }
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner (System.in);
         MenuPrinter intro = new MenuPrinter();
         Inventory inventory = new Inventory();
@@ -170,6 +170,6 @@ public class MenuPrinter {
         	run = intro.printOptions(input, inventory, printer);
         	System.out.println();
         }
-      
+
     }
 }
